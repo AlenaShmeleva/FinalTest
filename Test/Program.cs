e9b85990f360fe494a25ce62e929c111de7810da
Alyona <nakamura-san@mail.ru>
2022-11-09 15:54:58 +0300

    Solution

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.Clear();
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше или равна 3 символам.
+
+Console.Clear();
 
 void InputArray(string[] array)
 {
@@ -8,9 +11,23 @@ void InputArray(string[] array)
         array[i] = Convert.ToString(Console.ReadLine());
     }
 }
+void NewArray (string [] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+        { 
+            if (array[i].Length < 4)
+                Console.Write($"{array[i]} ");  
+        }
+    Console.Write("]");
+}
+
+
 Console.WriteLine("Сколько элментов массива вы хотите ввести?");
 int M = Convert.ToInt32(Console.ReadLine());
 string [] array = new string[M];
 InputArray(array);
 Console.WriteLine();
 Console.WriteLine($"Введённый массив: [{string.Join(", ", array)}]");
+Console.Write("Исходный массив: ");
+NewArray(array);
